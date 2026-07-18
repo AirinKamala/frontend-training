57459e77251f0f8ace2ccc345aeba3ffe7551985
adisan103 <arwinsmith01@gmail.com>
2026-07-18 08:55:22 +0800

    feat : add storytime home page with scss styling

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h1>Storytime</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.home {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+
+  h1 {
+    font-size: 3rem;
+  }
+}
+</style>
